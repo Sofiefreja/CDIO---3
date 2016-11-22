416dad2e6cf8d58f513705e4980c61133ae960c0
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2016-11-22 11:14:57 +0100

    Made comments for the class Tax

--- 15_Del3/src/game/Tax.java
+++ 15_Del3/src/game/Tax.java
@@ -1,22 +1,32 @@
 package game;
 
 public class Tax extends Square{
-	int taxAmount;
-	int taxRate=10;
+	int taxAmount; //amount defined in constructor
+	int taxRate=10; //rate 
+	/**
+	 * Constructor which accepts two parameters name and taxAmount for this specific instance.
+	 * @param name
+	 * @param taxAmount
+	 */
 	public Tax(String name, int taxAmount) {
 		super(name);
 		this.taxAmount=taxAmount;
 	}
+	/**
+	 * method for getting taxAmount.
+	 * @return taxAmount
+	 */
 	public int getTaxAmount(){
 		return taxAmount;
 	}
+	
 	@Override
 	public void landOnSquare(Player player) {
 		String ask;
 		ask=GUIControl.rateOrAmount(this);
-		if(ask.equals("Rate")){
-			player.pay((player.getBalance()/10));
-		}else{
+		if(ask.equals("Rate")){//if the player chose to pay the 10 % tax rate, this is calculated and payed.
+			player.pay((player.getBalance()/taxRate));
+		}else{ //else the taxAmount for this instance is payed.
 			player.pay(taxAmount);
 		}
 	}
