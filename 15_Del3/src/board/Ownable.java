b3c61f9c91c94d45474d9e6a828fedbc36e4efab
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2016-11-22 12:44:09 +0100

    made new package

---
+++ 15_Del3/src/board/Ownable.java
@@ -0,0 +1,51 @@
+package board;
+import game.Player;
+/**
+ * Abstract class Ownable, extended from Square. 
+ * Superclass to all ownable types of Square.
+ * @author Mathias Tværmose Gleerup
+ *
+ */ 
+public abstract class Ownable extends Square {
+	/**
+	 * Adds a field Player and int for all ownable squares.
+	 */
+	protected Player owner;
+	protected int price;
+	/**
+	 * Constructor which takes a name and a price of the square as parameters.
+	 * @param name
+	 * @param price
+	 */
+	public Ownable(String name, int price){
+		super(name);
+		this.price=price;
+	}
+	/**
+	 * Method for getting the rent of this instance.
+	 * @return rent.
+	 */
+	public abstract int getRent();
+	
+	/**
+	 * Method for clearing the owner field in this instance.
+	 */
+	public void clearOwner(){
+		this.owner=null;
+	}
+	
+	@Override
+	//Implements the inherited method landOnSquare, to be overridden for each type with a different getRent() method.
+	public void landOnSquare(Player player) {
+		if (owner != null) {
+			player.pay(getRent());
+			owner.deposit(getRent());
+		} else {
+			if (GUIControl.getBuyChoice(this) == true) {
+				player.bought(this);
+				player.pay(price);
+				owner = player;
+			}
+		}
+	}
+}
---
+++ 15_Del3/src/board/Square.java
@@ -0,0 +1,30 @@
+package board;
+import game.Player;
+/**
+ * Abstract class Square, superclass to all Squares.
+ * @author Mathias Tværmose Gleerup
+ *
+ */
+public abstract class Square {
+	protected String name;
+	
+	/**
+	 * Super constructor which takes a String name as a parameter.
+	 * @param name
+	 */
+	public Square( String name){
+		this.name=name;
+	}
+	/**
+	 * Returns the name of the Square
+	 * @return name
+	 */
+	public String toString(){
+		return name;
+	}
+	/**
+	 * Method which determines what happens to a player when he lands on this instance.
+	 * @param player
+	 */
+	public abstract void landOnSquare(Player player);
+}
