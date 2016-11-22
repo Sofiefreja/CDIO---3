1730abc913e110f01dbae3382b64df730885990b
Sofie <Sofie@82.211.217.173>
2016-11-22 17:31:58 +0100

    All sorts of stuff changed

--- 15_Del3/src/game/Board.java
+++ 15_Del3/src/game/Board.java
@@ -10,27 +10,27 @@ public class Board {
 	 * An array of the type square. In these indexes, It's defined which type
 	 * of square it is, and prices and rents of the different squares.  
 	 */
-	squares[0] = new Territory("Tribe Encampment", 1, 1000, 100);
-	squares[1] = new Fleet("Second Sail", 2, 4000);
-	squares[2] = new Territory("Crater", 3,  1500, 300);
-	squares[3] = new Refuge("Walled city", 4, 5000);
-	squares[4] = new LaborCamp("Huts in the mountain", 5, 2500);
-	squares[5] = new Territory("Mountain", 6, 2000, 500);
-	squares[6] = new Fleet("Sea Grover", 7, 4000);
-	squares[7] = new Territory("Cold Desert", 8, 3000, 700);
-	squares[8] = new Territory("Black Cave", 9, 4000, 1000);
-	squares[9] = new Tax("Goldmine", 10, 2000);
-	squares[10] = new Territory("Werewall", 11, 4300, 1300);
-	squares[11] = new Territory("Mountain Village", 12, 4750, 1600);
-	squares[12] = new Fleet("Buccaneers", 13, 4000);
-	squares[13] = new Territory("South Citadel", 14, 5000, 2000);
-	squares[14] = new Refuge("Monastery", 15, 500);
-	squares[15] = new LaborCamp("The pit", 16, 2500);
-	squares[16] = new Territory("Palace Gates", 17, 5500, 2600);
-	squares[17] = new Fleet("Privateer armada", 18, 4000);
-	squares[18] = new Tax("Caravan", 19, 4000);
-	squares[19] = new Territory("Tower", 20, 6000, 3200);
-	squares[20] = new Territory("Castle", 21, 8000, 4000);
+	squares[0] = new Territory("Tribe Encampment", 0, 1000, 100);
+	squares[1] = new Fleet("Second Sail", 1, 4000);
+	squares[2] = new Territory("Crater", 2,  1500, 300);
+	squares[3] = new Refuge("Walled city", 3, 5000);
+	squares[4] = new LaborCamp("Huts in the mountain", 4, 2500);
+	squares[5] = new Territory("Mountain", 5, 2000, 500);
+	squares[6] = new Fleet("Sea Grover", 6, 4000);
+	squares[7] = new Territory("Cold Desert", 7, 3000, 700);
+	squares[8] = new Territory("Black Cave", 8, 4000, 1000);
+	squares[9] = new Tax("Goldmine", 9, 2000);
+	squares[10] = new Territory("Werewall", 10, 4300, 1300);
+	squares[11] = new Territory("Mountain Village", 11, 4750, 1600);
+	squares[12] = new Fleet("Buccaneers", 12, 4000);
+	squares[13] = new Territory("South Citadel", 13, 5000, 2000);
+	squares[14] = new Refuge("Monastery", 14, 500);
+	squares[15] = new LaborCamp("The pit", 15, 2500);
+	squares[16] = new Territory("Palace Gates", 16, 5500, 2600);
+	squares[17] = new Fleet("Privateer armada", 17, 4000);
+	squares[18] = new Tax("Caravan", 18, 4000);
+	squares[19] = new Territory("Tower", 19, 6000, 3200);
+	squares[20] = new Territory("Castle", 20, 8000, 4000);
 	}
 	/*
 	 * Method to retrieve information, from one specific square.
--- 15_Del3/src/game/Game.java
+++ 15_Del3/src/game/Game.java
@@ -1,8 +1,43 @@
 package game;
+import java.util.ArrayList;
+import desktop_resources.GUI;
+import board.*;
 
 public class Game {
 	public Game(){
-	GUIControl hallo = new GUIControl();
-	hallo.makeBoard();
+		/*
+		 * Initializing the game.
+		 */
+		GUIControl GUIGame = new GUIControl();
+		GUIGame.makeBoard();
+		Board theBoard = new Board();
+		Cup theCup = new Cup();
+		/*
+		 * The players are initialized
+		 */
+		String[] playerNames = GUIGame.numberOfPlayers();
+		/*
+		 * Creating the players. By using an array, the names are saved in each of 
+		 * their own index'es in the array.
+		 */
+		ArrayList<Player> thePlayers = new ArrayList<Player>();
+		for(int i = 0; i < playerNames.length; i++){
+			thePlayers.add(new Player(playerNames[i]));
+		}
+		/*
+		 * Beginning the game. 
+		 */
+		String turn = GUIGame.getUserInputTurn(thePlayers.get(0));
+		if(turn.equals("roll")){
+			theCup.roll();
+			GUIGame.showDice(theCup);
+			thePlayers.get(0).moveCar(theCup.getSum());
+			GUIGame.moveVehicle(thePlayers.get(0));
+			int newPosition = thePlayers.get(0).getCurrentPosition();
+			Square currentSquare = theBoard.getSquare(newPosition);
+			currentSquare.landOnSquare(thePlayers.get(0));
+		} else if(turn.equals("Surrender")){
+			
+		}
 	}
 }
