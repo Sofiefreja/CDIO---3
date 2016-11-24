4a1002a6f70aa2623db999bfbfb1e5b0a406bddd
s153120 <s153120@DTU-CZC6268C6B.win.dtu.dk>
2016-11-24 14:02:25 +0100

    handled not enough money cases

--- 15_Del3/src/board/Tax.java
+++ 15_Del3/src/board/Tax.java
@@ -1,44 +1,66 @@
 package board;
+
 import game.Player;
 import game.GUIControl;
+
 /**
  * Class Tax extended from Square.
+ * 
  * @author Mathias Tværmose Gleerup
  *
  */
-public class Tax extends Square{
-	private int taxAmount; //amount defined in constructor
-	private int taxRate=10; //rate 
+public class Tax extends Square {
+	private int taxAmount; // amount defined in constructor
+	private int taxRate = 10; // rate
+
 	/**
-	 * Constructor which accepts two parameters name and taxAmount for this specific instance.
+	 * Constructor which accepts two parameters name and taxAmount for this
+	 * specific instance.
+	 * 
 	 * @param name
 	 * @param id
 	 * @param taxAmount
 	 */
-	public Tax(String name,int id, int taxAmount) {
-		super(name,id);
-		this.taxAmount=taxAmount;
+	public Tax(String name, int id, int taxAmount) {
+		super(name, id);
+		this.taxAmount = taxAmount;
 	}
+
 	/**
 	 * method for getting taxAmount.
+	 * 
 	 * @return taxAmount
 	 */
-	public int getTaxAmount(){
+	public int getTaxAmount() {
 		return taxAmount;
 	}
-	
+
 	@Override
 	public void landOnSquare(Player player) {
-		String ask;
-		ask=GUIControl.rateOrAmount(this,player);//Asks the player whether he wants to pay the 10 percent tax rate or the set tax amount.
-		if(ask.equals("Tax rate")){//if the player chose to pay the 10 % tax rate, this is calculated and payed.
-			int pay =player.getBalance()/taxRate;
+		
+		if (player.getBalance() > taxAmount) {
+			String ask;
+			ask = GUIControl.rateOrAmount(this, player);// Asks the player
+														// whether he wants to
+														// pay the 10 percent
+														// tax rate or the set
+														// tax amount.
+			if (ask.equals("Tax rate")) {// if the player chose to pay the 10 %
+											// tax rate, this is calculated and
+											// payed.
+				int pay = player.getBalance() / taxRate;
+				player.pay(pay);
+				GUIControl.taxMessage(player, pay);
+			} else if (ask.equals("Tax Amount")) { // else the taxAmount for
+													// this instance is payed.
+				player.pay(taxAmount);
+				GUIControl.taxMessage(player, taxAmount);
+				GUIControl.updateBalance(player);
+			}
+		} else {
+			GUIControl.taxNotEnough(this);
+			int pay = player.getBalance()/taxRate;
 			player.pay(pay);
-			GUIControl.taxMessage(player, pay);
-			GUIControl.updateBalance(player);
-		}else if(ask.equals("Tax Amount")){ //else the taxAmount for this instance is payed.
-			player.pay(taxAmount);
-			GUIControl.taxMessage(player, taxAmount);
 			GUIControl.updateBalance(player);
 		}
 	}
